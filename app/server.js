efa6a8216b0735cf869a8713f77a0e52c751caee
YMankivska <YMankivska@wsgc.com>
2022-03-21 19:23:25 +0200

    Added dist

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json'); 
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+    static: "./dist"
+});
 const port = process.env.PORT || 3200; 
 
 server.use(middlewares);
